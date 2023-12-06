b53c46110f6f936b25ad59dda72fd8bd649fd2be
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2023-12-06 16:37:14 -0600

    added pokemon,deck, and dashboard routes to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,15 @@ const router = require('express').Router();
 const userRoutes = require('./userroutes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const deckRoutes = require('./deck-routes'); // Add this line
+const pokemonRoutes = require('./pokemonroutes'); // Add this line
+const dashboardRoutes = require('./dashboard-routes'); // Add this line
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
 router.use('/comment', commentRoutes);
+router.use('/deck', deckRoutes); // Add this line
+router.use('/pokemon', pokemonRoutes); // Add this line
+router.use('/dashboard', dashboardRoutes); // Add this line
 
 module.exports = router;
