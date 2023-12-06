afbf418b00699ccabc620381424a95c2a37dbdb0
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2023-12-06 16:37:59 -0600

    deleted pokemon-routes.js

--- controllers/api/pokemon-routes.js
+++
@@ -1,7 +0,0 @@
-//Get a card
-//Fetch the details of a single card.
-//GET https://api.pokemontcg.io/v2/cards/<id>
-
-//Search cards
-//Search for one or many cards given a search query.
-//GET https://api.pokemontcg.io/v2/cards
