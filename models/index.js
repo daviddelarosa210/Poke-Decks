7631ec4afc74a974989c6b0802560392ffa97cba
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2023-12-06 16:40:51 -0600

    changed index.js in models to accept comments and posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
-// model/index.js
 const sequelize = require('../config/connection');
 const User = require('./User')(sequelize);
 const Deck = require('./Deck')(sequelize);
@@ -6,6 +5,8 @@ const Card = require('./Card')(sequelize);
 const DeckCard = require('./DeckCard')(sequelize);
 const UserDeck = require('./UserDeck')(sequelize);
 const Message = require('./Message')(sequelize);
+const Comment = require('./Comments')(sequelize); 
+const Post = require('./Post')(sequelize); 
 
 // Associations to allow users to own decks, and cards within those decks 
 Deck.belongsTo(User, { foreignKey: 'userId', onDelete: 'CASCADE' });
@@ -15,10 +16,15 @@ Deck.belongsToMany(User, { through: 'UserDeck' });
 Card.belongsToMany(Deck, { through: 'DeckCard' });
 Deck.belongsToMany(Card, { through: 'DeckCard' });
 
-//Associations to allow messages between users
+// Associations for comments and posts
+Comment.belongsTo(User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+Comment.belongsTo(Post, { foreignKey: 'postId', onDelete: 'CASCADE' });
+Post.belongsTo(User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+Post.hasMany(Comment, { foreignKey: 'postId' });
+
+// Associations to allow messages between users
 Message.belongsTo(User, { as: 'sender', foreignKey: 'senderId', onDelete: 'CASCADE' });
 Message.belongsTo(User, { as: 'receiver', foreignKey: 'receiverId', onDelete: 'CASCADE' });
-
 
 module.exports = {
   User,
@@ -26,4 +32,7 @@ module.exports = {
   Card,
   DeckCard,
   UserDeck,
+  Message,
+  Comment,
+  Post,
 };
